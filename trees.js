440f54caefe1ed80b8e98cb4727d7cb2c2556372
Guilherme Behs <guilhermebehs@192.168.68.106>
2023-11-13 11:56:21 -0300

    feat: trees example

---
+++ trees.js
@@ -0,0 +1,134 @@
+class Node{
+    _value = null
+    _left = null
+    _right = null
+
+    constructor(value){
+      this._value = value
+    }
+
+    get value(){
+      return this._value
+    }
+
+    set value(value){
+      this._value = value
+    }
+
+    get left(){
+      return this._left
+    }
+
+    set left(left){
+      this._left = left
+    }
+
+    get right(){
+      return this._right
+    }
+
+    set right(right){
+      this._right = right
+    }
+
+}
+
+
+class BinarySearchTree{
+    _root = null
+
+    constructor(root){
+      this._root = root
+    }
+
+    addNode(node){
+       if(this._root === null){
+         this._root = node
+         return
+       }
+       let currentNode = this._root
+       do{ 
+         if(currentNode.value > node.value){
+            if(currentNode.left === null){
+              currentNode.left = node
+              break
+            } else{
+                currentNode = currentNode.left
+            }
+         }
+         else{
+            if(currentNode.right === null){
+              currentNode.right = node
+              break
+            }
+            else{
+              currentNode = currentNode.right
+            }
+         }
+
+       }while(currentNode !== null)
+    }
+
+    printPreorder(){
+        if(this._root === null)
+            return
+        let currentNode = this._root
+        this._preorder(currentNode)
+    }
+
+    printPostorder(){
+        if(this._root === null)
+            return
+        let currentNode = this._root
+        this._postorder(currentNode)
+    }
+
+    printInorder(){
+        if(this._root === null)
+            return
+        let currentNode = this._root
+        this._inorder(currentNode)
+    }
+
+    _preorder(node){
+        if(node === null)
+            return
+        console.log(node.value)
+        this._preorder(node.left)
+        this._preorder(node.right)
+    }
+
+    _postorder(node){
+        if(node === null)
+            return
+        this._postorder(node.left)
+        this._postorder(node.right)
+        console.log(node.value)
+    }
+
+    _inorder(node){
+        if(node === null)
+            return
+        this._inorder(node.left)
+        console.log(node.value)
+        this._inorder(node.right)
+    }
+    
+}
+
+const root = new Node(100)
+const tree = new BinarySearchTree(root)
+
+tree.addNode(new Node(20))
+tree.addNode(new Node(200))
+tree.addNode(new Node(10))
+tree.addNode(new Node(30))
+tree.addNode(new Node(150))
+tree.addNode(new Node(300))
+
+console.log('\nPre Order')
+tree.printPreorder()
+console.log('\nPost Order')
+tree.printPostorder()
+console.log('\nIn Order')
+tree.printInorder()
