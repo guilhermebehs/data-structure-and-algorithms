d9f88033d5122929206af028c16e09cc787744e5
Guilherme Behs <guilhermebehs@192.168.68.105>
2024-01-23 09:08:53 -0300

    feat: improve binary search

--- binary-search.js
+++ binary-search.js
@@ -4,18 +4,18 @@ const list = Array.from(Array(100000).keys())
 function binarySearch(number, inputList){
     let lowestIndex = 0
     let highestIndex = inputList.length - 1
-    let middle = Math.floor((lowestIndex + highestIndex) / 2) 
+    let middle = null
     
     for(let i=0; i < inputList.length; i++){
-         if(number === inputList[middle])
-           return {number, tries:i+1}
+        middle = Math.floor((lowestIndex + highestIndex) / 2)
+        if(number === inputList[middle])
+           return number
         if(number > inputList[middle]){
             lowestIndex = middle + 1  
         }
         else{
             highestIndex = middle -1
         }
-        middle = Math.floor((lowestIndex + highestIndex) / 2)
     }
 } 
 
@@ -25,16 +25,13 @@ function sequentialSearch(number, inputList){
           return number
 }
 
-console.log('Binary Search')
-console.time()
+console.time('Binary Search')
 for(let i = 1; i < list.length; i++)
     binarySearch(i,list)
 
-console.timeEnd()
+console.timeEnd('Binary Search')
 
-console.log('Sequential Search')
-console.time()
+console.time('Sequential Search')
 for(let i = 1; i < list.length; i++)
     sequentialSearch(i,list)
-
-console.timeEnd()
+console.timeEnd('Sequential Search')
